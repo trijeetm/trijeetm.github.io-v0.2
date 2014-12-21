e6568129847056a713aff7abb18115cc1a922466
trijeetm <trijeetm@gmail.com>
2014-12-21 15:52:55 -0800

    Added animate.css and query binding for animations

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+
+$(document).ready(function() {
+	var elems = $('.fadeUp');
+	for (var i = 0; i < elems.length; i++) {
+		$(elems[i]).addClass('animated');
+		$(elems[i]).addClass('fadeInUp');
+	};
+});
