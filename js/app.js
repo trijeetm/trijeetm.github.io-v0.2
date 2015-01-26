7dc7ef15beaea76d93f405d35e943656844a91d0
trijeetm <trijeetm@gmail.com>
2015-01-26 03:21:05 -0800

    Designed inner portfolio pages

--- js/app.js
+++ js/app.js
@@ -1,9 +1,38 @@
 $(document).foundation();
 
+// $(document).scroll(function() {
+// 	var elems = $('.fadeUp');
+// 	elems.each(function(index, el) {
+// 		var elemPos = $(el).offset().top + ($(el).outerHeight()* 2 / 4);
+//         var bottomOfWindow = $(window).scrollTop() + $(window).height();
+//         console.log("bottomOfWindow " + bottomOfWindow);
+//         console.log(elemPos);
+// 		if (elemPos < bottomOfWindow) {
+// 			// console.log("fading in elem" + index);
+// 			setTimeout(function() {
+// 				$(el).addClass('animated');
+// 				$(el).addClass('fadeInUp');
+// 			}, 100);
+//         }
+// 		// setTimeout(function() {
+// 		// 	$(el).addClass('animated');
+// 		// 	$(el).addClass('fadeInUp');
+// 		// }, 1000 * index);
+// 	});
+// });
+
 $(document).ready(function() {
 	var elems = $('.fadeUp');
-	for (var i = 0; i < elems.length; i++) {
-		$(elems[i]).addClass('animated');
-		$(elems[i]).addClass('fadeInUp');
-	};
+	elems.each(function(index, el) {
+		$(el).addClass('animated');
+		$(el).addClass('fadeInUp');
+	});
+});
+
+$('#close-project').click(function(event) {
+	event.preventDefault();
+	$('.container').fadeOut('500');
+	setTimeout(function() {
+		$(location).attr('href', 'index.html#portfolio');
+	}, 500);
 });
